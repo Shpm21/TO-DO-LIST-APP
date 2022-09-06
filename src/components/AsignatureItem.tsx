cd94e60c04df22a1cb96e11b3ba58f3f7f9c89b0
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-05 21:00:23 -0400

    create asignaturaItem component

--- src/components/AsignatureItem.tsx
+++ src/components/AsignatureItem.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { IonItem, IonLabel } from "@ionic/react";
+import { IonButton, IonItem, IonLabel } from "@ionic/react";
 
 import { Asignature } from "../models/asignature.model";
+import { useLocation, useParams } from "react-router";
+import { useStorage } from "../useStorage";
 
 const AsignatureItem: React.FC<{asignature: Asignature}> = ({asignature}) => {
+
+    const { deleteAsignature } = useStorage();
+    const deleteA = async (name: string) => {
+        await deleteAsignature(name);
+    }
+    const location = useLocation();
+    const pathPag: string = `/page/${asignature.name}`;
     return (
         <IonItem>
-            <IonLabel>{asignature.name}</IonLabel>
+
+            <IonItem className={location.pathname === pathPag ? 'selected': ''} routerLink={pathPag}>
+                <IonLabel>{asignature.name}</IonLabel>
+            </IonItem>
+            <IonItem >
+                <IonButton onClick={ () => deleteA(asignature.name)} >x</IonButton>
+            </IonItem>
         </IonItem>
     );
 }
