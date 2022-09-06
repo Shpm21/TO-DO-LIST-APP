9804054733fb0f57ea12f94c4365e0fd3b076acc
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-05 21:02:37 -0400

    modify main page

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,12 +1,8 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButtons, IonContent, IonHeader, IonInput, IonItem, IonList, IonMenuButton, IonPage, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/react';
+import ViewToDoListItem from '../components/ViewToDoListItem';
 import './Page.css';
 
 const Page: React.FC = () => {
-
-  const { name } = useParams<{ name: string; }>();
-
   return (
     <IonPage>
       <IonHeader>
@@ -14,19 +10,19 @@ const Page: React.FC = () => {
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
-          <IonTitle>{name}</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
-
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">{name}</IonTitle>
+            <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={name} />
+        <ViewToDoListItem />
       </IonContent>
     </IonPage>
+
   );
 };
 
