2a58679b5ce76408401be8e3d79bb9f74e8e75f1
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-05 21:02:04 -0400

    Create addAsignaturePage page

---
+++ src/pages/AddAsignaturePage.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useParams } from "react-router";
+import AddAsignatureItem from "../components/AddAsignatureItem";
+
+const AddAsignaturePage: React.FC = () => {
+    const { name } = useParams<{ name: string; }>();
+    
+    return (
+        <IonPage>
+        <IonHeader>
+            <IonToolbar>
+                <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+            <IonTitle>{name}</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+            <IonHeader collapse="condense">
+            <IonToolbar>
+                <IonTitle size="large">{name}</IonTitle>
+            </IonToolbar>
+            </IonHeader>
+        <AddAsignatureItem />
+        </IonContent>
+        </IonPage>
+    );
+}
+
+export default AddAsignaturePage;
