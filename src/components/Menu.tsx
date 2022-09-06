901e643dc675c61495a65b32184471f8b3f6f216
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-05 21:00:59 -0400

    delete local database to file menu tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,5 @@
 import {
+  IonButton,
   IonContent,
   IonIcon,
   IonItem,
@@ -10,141 +11,70 @@ import {
   IonNote,
 } from '@ionic/react';
 
-import { useLocation } from 'react-router-dom';
 import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
 import './Menu.css';
-import { Asignature } from '../models/asignature.model';
 import AsignatureItem from './AsignatureItem';
+import { Asignature } from "../models/asignature.model";
+import { useEffect, useState } from 'react';
+import { useStorage } from '../useStorage';
 
-interface AppPage {
-  url: string;
-  iosIcon: string;
-  mdIcon: string;
-  title: string;
-}
 
-const asignatures : Asignature[] = [
-  {
-    name: "App Dist I",
-    credit: 5
-  },
-  {
-    name: "Emprendimiento",
-    credit: 2
-  },
-  {
-    name: "Com Efect II",
-    credit: 2
-  },
-  {
-    name: "Dibujo de Ing",
-    credit: 5
-  },
-  {
-    name: "Electivo",
-    credit: 5
-  }, 
-  {
-    name: "Proyecto Electivo",
-    credit: 5
-  },
-  {
-    name: "Etica",
-    credit: 1
-  },
-  {
-    name: "Ing de Datos I",
-    credit: 5
-  }
-]
 
-const appPages: AppPage[] = [
-  {
-    title: 'Inbox',
-    url: '/page/Inbox',
-    iosIcon: mailOutline,
-    mdIcon: mailSharp
-  },
-  {
-    title: 'Outbox',
-    url: '/page/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
-  },
-  {
-    title: 'Favorites',
-    url: '/page/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
-  },
-  {
-    title: 'Archived',
-    url: '/page/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
-  },
-  {
-    title: 'Trash',
-    url: '/page/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/page/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
-  }
-];
 const Menu: React.FC = () => {
-  const location = useLocation();
+  const [asignatures, setAsignatures] = useState<Asignature[]>([]);
+  const { getAllAsignatures, asignature} = useStorage();
+
+  useEffect(() => {
+    const actionGetAllAsignatures = async () => {
+      const allAsignatures = await getAllAsignatures();
+      setAsignatures(allAsignatures);
+    }
+    actionGetAllAsignatures();
+  }, [asignatures]);
+
+
 
   return (
     <IonMenu contentId="main" type="overlay">
       <IonContent>
+      <IonList id="inbox-list">
+          <IonListHeader>Actions</IonListHeader>
+          <IonMenuToggle autoHide={false}>
+            <IonItem routerLink='/home'>
+              <IonLabel>
+                  Home
+                </IonLabel>            
+            </IonItem>
+          </IonMenuToggle>
+          <IonMenuToggle autoHide={false}>
+            <IonItem routerLink='/addTask'>
+              <IonLabel>
+                Add Task
+              </IonLabel>
+            </IonItem>
+          </IonMenuToggle>
+          <IonMenuToggle autoHide={false}>
+            <IonItem routerLink='/addAsignature'>
+              <IonLabel>
+                Add Asignature
+              </IonLabel>
+            </IonItem>
+          </IonMenuToggle>
+        </IonList>
         <IonList id="inbox-list">
           <IonListHeader>Asignaturas</IonListHeader>
           {asignatures.map((asign, index) => {
             return (
               <IonMenuToggle key={index} autoHide={false}>
-                <AsignatureItem asignature={asign} />
+                <AsignatureItem asignature={asign}  />
               </IonMenuToggle>
             );
           })}
         </IonList>
       </IonContent>
+
     </IonMenu>
   );
 };
 
-// return (
-//   <IonMenu contentId="main" type="overlay">
-//     <IonContent>
-//       <IonList id="inbox-list">
-//         <IonListHeader>Inbox</IonListHeader>
-//         <IonNote>hi@ionicframework.com</IonNote>
-//         {appPages.map((appPage, index) => {
-//           return (
-//             <IonMenuToggle key={index} autoHide={false}>
-//               <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-//                 <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
-//                 <IonLabel>{appPage.title}</IonLabel>
-//               </IonItem>
-//             </IonMenuToggle>
-//           );
-//         })}
-//       </IonList>
-
-//       <IonList id="labels-list">
-//         <IonListHeader>Labels</IonListHeader>
-//         {labels.map((label, index) => (
-//           <IonItem lines="none" key={index}>
-//             <IonIcon slot="start" icon={bookmarkOutline} />
-//             <IonLabel>{label}</IonLabel>
-//           </IonItem>
-//         ))}
-//       </IonList>
-//     </IonContent>
-//   </IonMenu>
-// );
 export default Menu;
