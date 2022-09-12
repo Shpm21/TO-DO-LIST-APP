99d113db874ac1e4a0aa1366e434c7d760e2e14a
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-11 21:49:52 -0400

    modify page element

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,8 +1,34 @@
 import { IonButtons, IonContent, IonHeader, IonInput, IonItem, IonList, IonMenuButton, IonPage, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/react';
+import { useEffect, useState } from 'react';
 import ViewToDoListItem from '../components/ViewToDoListItem';
+import { Task } from '../models/task.model';
+import { useStorage2 } from '../useStorage2';
 import './Page.css';
 
+const LoadTasks = async () => {
+  const { getAllTask } = useStorage2();
+  const tasks: Task[] = await getAllTask();
+  return tasks!;
+}
+
+
 const Page: React.FC = () => {
+  const [allTaskA, setAllTaskA]  = useState<Task[]>([]);
+  const { getAllTask } = useStorage2();
+
+  const loadTasks = async () => {
+    const tasks: Task[] = await getAllTask();
+    setAllTaskA(tasks!);
+  }
+  loadTasks();
+  useEffect(() => {
+    const acctionGetAllTask = async () => {
+      const tasks = await getAllTask();
+      setAllTaskA(tasks);
+    }
+    acctionGetAllTask();
+  });
+//ACA QUEDE
   return (
     <IonPage>
       <IonHeader>
@@ -10,16 +36,20 @@ const Page: React.FC = () => {
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
-          <IonTitle>Home</IonTitle>
+          <IonTitle onClick={async () => {alert(`TASKS: ${JSON.stringify(setAllTaskA(await getAllTask()))}`)}}>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Home</IonTitle>
+            <IonTitle size="large" onClick={() => {alert(`TASKS: ${JSON.stringify(allTaskA)}`)}}>Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ViewToDoListItem />
+        {
+          allTaskA.length > 0 ?
+          <ViewToDoListItem tasks={allTaskA} /> :
+          <p> No hay tareas</p>
+        }
       </IonContent>
     </IonPage>
 
