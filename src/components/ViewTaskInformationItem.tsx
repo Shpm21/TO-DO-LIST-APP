1be45286ece43f20581b89299e0543b60a2d1cb6
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-11 21:49:28 -0400

    create ViewTaskInformationItem component

--- src/components/ViewTaskInformationItem.tsx
+++ src/components/ViewTaskInformationItem.tsx
@@ -1,3 +1,33 @@
-<></>
+import { IonButton, IonContent, IonItem, IonLabel, IonPopover } from "@ionic/react";
+import { Task } from "../models/task.model";
 
-// const ViewTaskInformationItem 
+interface Props {
+    task : Task;
+    key: number;
+}
+
+const ViewTaskInformationItem: React.FC<Props> = (Props) => {
+    let idButton: string = 'trigger' + Props.key;
+    return (
+        <IonItem>
+            <IonButton id={idButton}>view</IonButton>
+            <IonPopover trigger={idButton}>
+                <IonItem>
+                    <IonLabel>Nombre: {Props.task.name}</IonLabel>
+                </IonItem>
+                <IonItem>
+                    <IonLabel>Asignatura: {Props.task.nameAsignature}</IonLabel>
+                </IonItem>
+                <IonItem>
+                    <IonLabel>Descripción: {Props.task.description}</IonLabel>
+                </IonItem>
+                <IonItem>
+                    <IonLabel>Fecha de entrega: {Props.task.date}</IonLabel>
+                </IonItem>
+            </IonPopover>
+        </IonItem>
+
+    );
+}
+
+export default ViewTaskInformationItem; 
