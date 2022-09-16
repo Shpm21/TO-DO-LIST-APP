daef0e2b524579bb2edbd511de45bb8cce5e45c9
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-16 17:30:34 -0400

    add tab bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,79 @@
-import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router-dom';
-import Menu from './components/Menu';
-import Page from './pages/Page' ;
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonSplitPane,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  setupIonicReact,
+} from '@ionic/react'
+import { IonReactRouter } from '@ionic/react-router'
+import { Redirect, Route } from 'react-router-dom'
+import Menu from './components/Menu'
+import AsignaturePage from './pages/AsignaturePage'
+import AddItemPage from './pages/AddItemPage'
+import AddAsignaturePage from './pages/AddAsignaturePage'
+import TaskPage from './pages/TaskPage'
+import Home from './pages/Home'
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import '@ionic/react/css/core.css'
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import '@ionic/react/css/normalize.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/typography.css'
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import '@ionic/react/css/padding.css'
+import '@ionic/react/css/float-elements.css'
+import '@ionic/react/css/text-alignment.css'
+import '@ionic/react/css/text-transformation.css'
+import '@ionic/react/css/flex-utils.css'
+import '@ionic/react/css/display.css'
 
 /* Theme variables */
-import './theme/variables.css';
-import AsignaturePage from './pages/AsignaturePage';
-import AddItemPage from './pages/AddItemPage';
-import AddAsignaturePage from './pages/AddAsignaturePage';
-import TaskPage from './pages/TaskPage';
+import './theme/variables.css'
+import './theme/iontab.css'
+import AsignaturesPage from './pages/AsignaturesPage'
+import ActionsPage from './pages/ActionsPage'
 
-
-setupIonicReact();
+setupIonicReact()
 
 const App: React.FC = () => {
-
   return (
     <IonApp>
       <IonReactRouter>
-        <IonSplitPane contentId="main">
-          <IonRouterOutlet id="main" >
-            <Route path="/home" component={Page} />
-            <Route path="/" exact={true}>
-              <Redirect to="/home"/>
-            </Route>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route path="/home" component={Home} exact={true} />
+            <Route path="/actions" component={ActionsPage} exact={true} />
+            <Route path="/asignatures" component={AsignaturesPage} />
             <Route path="/page/:name" exact={true} component={AsignaturePage} />
-            <Route path="/addTask" exact={true} component={AddItemPage} />
-            <Route path="/addAsignature" exact={true} component={AddAsignaturePage} />
             <Route path="/task/:id" exact={true} component={TaskPage} />
+            <Route
+              path="/"
+              render={() => <Redirect to="/home" />}
+              exact={true}
+            />
           </IonRouterOutlet>
-          <Menu />
-        </IonSplitPane>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="actions" href="/actions">
+              <IonIcon name="add-circle" size="large"></IonIcon>
+            </IonTabButton>
+            <IonTabButton tab="/home" href="/home">
+              <IonIcon name="reader" size="large"></IonIcon>
+            </IonTabButton>
+            <IonTabButton tab="asignatures" href="/asignatures">
+              <IonIcon name="library" size="large"></IonIcon>
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
       </IonReactRouter>
     </IonApp>
-  );
-};
+  )
+}
 
-export default App;
+export default App
