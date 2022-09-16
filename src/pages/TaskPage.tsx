5003455b664b80f89e31fbf6d5896a2f0bdcd75f
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-16 17:35:37 -0400

    format file

--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -8,29 +8,29 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-} from "@ionic/react";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { Task } from "../models/task.model";
-import { useStorage2 } from "../useStorage2";
-import "./Page.css";
+} from '@ionic/react'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { Task } from '../models/task.model'
+import { useStorage2 } from '../useStorage2'
+import './Home.css'
 
 interface Props {
-  task: Task;
+  task: Task
 }
 
 const TaskPage: React.FC<Props> = (Props) => {
-  const [task, setTask] = useState<Task>();
-  const { id } = useParams<{ id: string }>();
-  const { getTaskById } = useStorage2();
+  const [task, setTask] = useState<Task>()
+  const { id } = useParams<{ id: string }>()
+  const { getTaskById } = useStorage2()
 
   useEffect(() => {
     const getTaskInformation = async () => {
-      const auxTask = await getTaskById(id);
-      setTask(auxTask![0]);
-    };
-    getTaskInformation();
-  }, [task]);
+      const auxTask = await getTaskById(id)
+      setTask(auxTask![0])
+    }
+    getTaskInformation()
+  }, [task])
 
   return (
     <IonPage>
@@ -52,12 +52,12 @@ const TaskPage: React.FC<Props> = (Props) => {
           <h2>Nombre: {task?.name}</h2>
           <h3>Fecha de entrega: {task?.date}</h3>
           <h3>Asignatura: {task?.nameAsignature}</h3>
-          <h3>Estado: {task?.done ? "Realizada" : "Pendiente"}</h3>
+          <h3>Estado: {task?.done ? 'Realizada' : 'Pendiente'}</h3>
           <p>Descripción: {task?.description}</p>
         </IonLabel>
       </IonContent>
     </IonPage>
-  );
-};
+  )
+}
 
-export default TaskPage;
+export default TaskPage
