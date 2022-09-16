5533d20a54cd6073f9e9fd01939da6f7792e0ddd
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-16 17:34:58 -0400

    modify

--- src/pages/AsignaturePage.tsx
+++ src/pages/AsignaturePage.tsx
@@ -1,11 +1,18 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ViewTaskFromAsignatureItem from '../components/ViewTaskFromAsignatureItem';
-import { useParams } from 'react-router';
-import './Page.css';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react'
+import ViewTaskFromAsignatureItem from '../components/ViewTaskFromAsignatureItem'
+import { useParams } from 'react-router'
+import './Home.css'
 
 const AsignaturePage: React.FC = () => {
-
-  const { name } = useParams<{ name: string; }>();
+  const { name } = useParams<{ name: string }>()
 
   return (
     <IonPage>
@@ -23,10 +30,10 @@ const AsignaturePage: React.FC = () => {
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ViewTaskFromAsignatureItem name={name}/>
+        <ViewTaskFromAsignatureItem name={name} />
       </IonContent>
     </IonPage>
-  );
-};
+  )
+}
 
-export default AsignaturePage;
+export default AsignaturePage
