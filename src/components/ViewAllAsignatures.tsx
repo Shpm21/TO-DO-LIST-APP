079a08c996104fb3469468ad322cc7bce5187e62
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-16 17:33:14 -0400

    create a ViewAllAsignatures Cmponent

---
+++ src/components/ViewAllAsignatures.tsx
@@ -0,0 +1,33 @@
+import { IonContent, IonList, IonListHeader, IonMenuToggle } from '@ionic/react'
+import { useEffect, useState } from 'react'
+import { Asignature } from '../models/asignature.model'
+import { useStorage2 } from '../useStorage2'
+import AsignatureItem from './AsignatureItem'
+
+const ViewAllAsignatures: React.FC = () => {
+  const [asignatures, setAsignatures] = useState<Asignature[]>([])
+  const { getAllAsignatures } = useStorage2()
+
+  useEffect(() => {
+    const actionGetAllAsignatures = async () => {
+      const allAsignatures = await getAllAsignatures()
+      setAsignatures(allAsignatures)
+    }
+    actionGetAllAsignatures()
+  })
+  return (
+    <IonContent>
+      <IonList id="inbox-list">
+        {asignatures.map((asign, index) => {
+          return (
+            <IonMenuToggle key={index} autoHide={false}>
+              <AsignatureItem asignature={asign} />
+            </IonMenuToggle>
+          )
+        })}
+      </IonList>
+    </IonContent>
+  )
+}
+
+export default ViewAllAsignatures
