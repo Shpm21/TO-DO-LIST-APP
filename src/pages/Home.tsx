fb03be54d55e29b4a8aac4e21ae19da61d0cdfcf
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-18 00:04:36 -0400

    create filters taks functions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import {
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
+  IonLabel,
   IonMenuButton,
   IonPage,
   IonTitle,
+  IonToggle,
   IonToolbar,
 } from '@ionic/react'
 import { useEffect, useState } from 'react'
@@ -13,9 +17,18 @@ import { Task } from '../models/task.model'
 import { useStorage2 } from '../useStorage2'
 import './Home.css'
 
+const filterAllDoneTask = (allTask: Task[]) => {
+  return allTask.filter((task) => task.done)
+}
+
+const filterAllDontDoneTask = (allTask: Task[]) => {
+  return allTask.filter((task) => !task.done)
+}
+
 const Home: React.FC = () => {
   const { getAllTask } = useStorage2()
   const [allTask, setTasks] = useState<Task[]>([])
+  const [viewTaskDone, setViewTaskDone] = useState<boolean>(false)
 
   useEffect(() => {
     const getTaskInformation = async () => {
@@ -33,6 +46,19 @@ const Home: React.FC = () => {
             <IonMenuButton />
           </IonButtons>
           <IonTitle>Tareas Pendientes</IonTitle>
+          {!viewTaskDone ? (
+            <IonIcon slot="end" name="eye-off" color="tertiary"></IonIcon>
+          ) : (
+            <IonIcon slot="end" name="eye" color="success"></IonIcon>
+          )}
+          <IonToggle
+            slot="end"
+            value="Ver tareas realizadas"
+            onClick={() => {
+              viewTaskDone ? setViewTaskDone(false) : setViewTaskDone(true)
+            }}
+            color="success"
+          />
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -41,8 +67,14 @@ const Home: React.FC = () => {
             <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        {allTask.length > 0 ? (
-          <ViewToDoListItem tasks={allTask} />
+        {viewTaskDone! ? (
+          allTask.length > 0 ? (
+            <ViewToDoListItem tasks={filterAllDoneTask(allTask)} />
+          ) : (
+            <p>No hay tareas disponibles</p>
+          )
+        ) : allTask.length > 0 ? (
+          <ViewToDoListItem tasks={filterAllDontDoneTask(allTask)} />
         ) : (
           <p>No hay tareas disponibles</p>
         )}
