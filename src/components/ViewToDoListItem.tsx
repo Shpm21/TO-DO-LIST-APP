32a0eef512831ce9baa539b888ec07773b7f130d
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-25 15:18:42 -0400

    Remove parameters to Task Item

--- src/components/ViewToDoListItem.tsx
+++ src/components/ViewToDoListItem.tsx
@@ -9,7 +9,6 @@ import {
 } from '@ionic/react'
 import { Task } from '../models/task.model'
 import { useStorage2 } from '../useStorage2'
-import { useLocation } from 'react-router'
 import TaskItem from './TaskItem'
 
 interface Props {
@@ -17,8 +16,6 @@ interface Props {
 }
 
 const ViewToDoListItem: React.FC<Props> = (Props) => {
-  const [taskAux, setTaskAux] = useState<Task>()
-  const [showPopover, setShowPopover] = useState(false)
   const { deleteTask } = useStorage2()
   return (
     <IonContent fullscreen>
@@ -26,31 +23,13 @@ const ViewToDoListItem: React.FC<Props> = (Props) => {
         {Props.tasks ? (
           Props.tasks.map((task, index) => {
             return (
-              <TaskItem
-                task={task}
-                index={index}
-                setTaskAux={setTaskAux}
-                setShowPopover={setShowPopover}
-                deleteTask={deleteTask}
-              />
+              <TaskItem task={task} index={index} deleteTask={deleteTask} />
             )
           })
         ) : (
           <></>
         )}
       </IonList>
-      {/* <IonPopover trigger={taskAux?.id} hidden={showPopover}>
-        {showPopover ? (
-          <></>
-        ) : (
-          <IonItem
-            className={location.pathname === pathPag ? 'selected' : ''}
-            routerLink={pathPag}
-          >
-            <IonLabel onClick={() => setShowPopover(true)}>Ver más...</IonLabel>
-          </IonItem>
-        )}
-      </IonPopover> */}
     </IonContent>
   )
 }
