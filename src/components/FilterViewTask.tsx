2d32aee7daad57288fd4dfecd0e1688cee565fd2
Shpm21 <galleguilloscristian2@gmail.com>
2022-09-25 17:48:37 -0400

    create FilterViewTask component

---
+++ src/components/FilterViewTask.tsx
@@ -0,0 +1,30 @@
+import { IonContent } from '@ionic/react'
+import { Task } from '../models/task.model'
+import { FilterTasksServices } from '../services/filterTasksServices'
+import ViewToDoListItem from './ViewToDoListItem'
+
+interface Props {
+  viewTaskDone: boolean
+  tasks: Task[]
+}
+
+const filterTask: FilterTasksServices = new FilterTasksServices()
+const FilterViewTask: React.FC<Props> = ({ viewTaskDone, tasks }) => {
+  return (
+    <IonContent>
+      {viewTaskDone! ? (
+        tasks.length > 0 ? (
+          <ViewToDoListItem tasks={filterTask.filterTasks(tasks, 'd')} />
+        ) : (
+          <p>No hay tareas disponibles</p>
+        )
+      ) : tasks.length > 0 ? (
+        <ViewToDoListItem tasks={filterTask.filterTasks(tasks, 'dd')} />
+      ) : (
+        <p>No hay tareas disponibles</p>
+      )}
+    </IonContent>
+  )
+}
+
+export default FilterViewTask
